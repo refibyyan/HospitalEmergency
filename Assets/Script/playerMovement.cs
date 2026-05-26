0c5f08909d74262f18903bd214b85f29acf5e169
Refi Abyyan <refiabyyan22@gmail.com>
2026-05-26 10:36:41 +0700

    Level 2 - Dialog Full
    Tinggal fix textnya yg kebawah sendiri, tambahin deciision, dan timer, dan masing masing endingnya, dan tambahin sfx background

--- Assets/Script/playerMovement.cs
+++ Assets/Script/playerMovement.cs
@@ -6,36 +6,66 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+
     private Rigidbody2D rb;
+
     private Vector2 moveInput;
+
     private Animator animator;
 
-    // Start is called before the first frame update
+    public bool canMove = true;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>(); // <-- Dibetulkan jadi A kapital
+
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // freeze movement
+        if (!canMove)
+        {
+            rb.linearVelocity = Vector2.zero;
+
+            animator.SetBool("isWalking", false);
+
+            return;
+        }
+
+        // movement
         rb.linearVelocity = moveInput * moveSpeed;
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        animator.SetBool("isWalking", true);
-
-        if (context.canceled)
+        // kalau freeze jangan baca input
+        if (!canMove)
         {
-            animator.SetBool("isWalking", false); // <-- Angka 0 dibuang jadi false saja
-            animator.SetFloat("LastInputX", moveInput.x);
-            animator.SetFloat("LastInputY", moveInput.y);
+            rb.linearVelocity = Vector2.zero;
+
+            animator.SetBool("isWalking", false);
+
+            return;
         }
+
         moveInput = context.ReadValue<Vector2>();
+
+        // walking animation
+        animator.SetBool("isWalking", moveInput != Vector2.zero);
+
+        // direction animation
         animator.SetFloat("InputX", moveInput.x);
+
         animator.SetFloat("InputY", moveInput.y);
 
+        // last direction
+        if (context.canceled)
+        {
+            animator.SetFloat("LastInputX", moveInput.x);
+
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
     }
 }
